25287b7de5cc618afb31693987f28a8e366faf44
krapovskik <krapovskik@gmail.com>
2022-01-31 20:31:47 +0100

    heroku change

--- src/main/resources/static/js/Map.js
+++ src/main/resources/static/js/Map.js
@@ -38,9 +38,9 @@ function filterStations(map){
     var filter = document.getElementById('filter');
     var ourRequest = new XMLHttpRequest();
     if(filter.value.length === 0)
-        ourRequest.open('GET','https://charging-stations-mk.herokuapp.com/api/stations')
+        ourRequest.open('GET','/api/stations')
     else
-        ourRequest.open('GET','https://charging-stations-mk.herokuapp.com/api/stations?filter='+filter.value)
+        ourRequest.open('GET','/api/stations?filter='+filter.value)
     ourRequest.onload = function (){
         var ourData = JSON.parse(ourRequest.responseText);
         var marker;
@@ -59,17 +59,13 @@ function filterStations(map){
 }
 
 function getCity(lat,lng) {
-    // var ourRequest = new XMLHttpRequest();
-    // ourRequest.open('GET', 'http://api.geonames.org/findNearbyPlaceNameJSON?lat='+ lat +'&lng='+ lng +'&username=dzemomusaka')
-    const url = 'http://api.geonames.org/findNearbyPlaceNameJSON?lat='+ lat +'&lng='+ lng +'&username=dzemomusaka';
-    // ourRequest.onload = function () {
-    //     var ourData = JSON.parse(ourRequest.responseText);
-    //     $('#city').val(ourData.geonames[0].adminName1+"");
-    // };
-    // ourRequest.send();
-    $.getJSON(url,function (data){
-        console.log(data);
-    })
+    var ourRequest = new XMLHttpRequest();
+    ourRequest.open('GET', 'https://api.openweathermap.org/geo/1.0/reverse?lat='+lat+'&lon='+lng+'&limit=2&appid=fb06a46815d7f7c87c719dcf9aa0fa71')
+    ourRequest.onload = function () {
+        var ourData = JSON.parse(ourRequest.responseText);
+        $('#city').val(ourData[0]['local_names']['en']);
+    };
+    ourRequest.send();
 }
 
 function showLatLng(){
